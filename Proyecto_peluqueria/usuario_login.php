bb71b70756dc7dd1362ca6b8f861868f99626310
luisillo <luisardilamacias@gmail.com>
2020-04-03 17:16:49 +0000

    login_usuario.php

--- Proyecto_peluqueria/usuario_login.php
+++ Proyecto_peluqueria/usuario_login.php
@@ -1,5 +1,5 @@
 <?php
-	header('Content-type: application/json');
+	header('Content-Type: application/json');
 	require 'BASE/BDD.php';
 	/*
 		Respuesta:
@@ -14,12 +14,46 @@
 
 	*/
 
-		$usuario = $_GET['usuario'];
-		$clave = $_GET['clave'];
+$usuario = $_GET['usuario'];
 
-	$data = BDD::QUERY("SELECT usuario,clave,descripcion as rol from usuario,rol where rol_id = id_rol and usuario='$usuario' and clave='$clave' ");
-	if ($data) {
-		echo json_encode($data, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_PRETTY_PRINT);
+$query = "SELECT id, usuario,clave,id_rol,usuario.estado,rol.descripcion,rol_id from usuario,rol where rol_id = id_rol and usuario='".$usuario."';";
+
+
+function disconnectDB($conexion){
+    $close = mysqli_close($conexion);
+        if($close){
+           // echo 'La desconexion de la base de datos se ha hecho satisfactoriamente';
+        }else{
+            echo 'Ha sucedido un error inexperado en la desconexion de la base de datos';
+        }   
+    return $close;
+}
+
+function getArraySQL($sql){
+    $conexion = BDD::CONECTAR();
+    if(!$result = mysqli_query($conexion, $sql)) die(); 
+    $rawdata = array();
+    $i=0;
+    while($row = mysqli_fetch_assoc($result))
+   	 	{
+        	$rawdata[$i] = $row;
+        	$i++;
+    	}
+    disconnectDB($conexion); 
+    return $rawdata;
+}
+
+$lst_usuario = getArraySQL($query);
+
+
+	if ($lst_usuario) {			
+		foreach ($lst_usuario as $v) {
+			$array = array("id"=>$v["id"],"usuario" => $v["usuario"],"clave"=>$v["clave"],"estado"=>$v["estado"],"rol_id"=>$v["rol_id"] ]);
+
+		}
+
+
+		echo json_encode($array, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_PRETTY_PRINT);	
 	}else{
 		echo json_encode("error", JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_PRETTY_PRINT);
 	}
--- Proyecto_peluqueria/usuarios.php
+++ Proyecto_peluqueria/usuarios.php
@@ -14,9 +14,16 @@
 
 	*/
 
-	$data = BDD::QUERY("SELECT u.usuario,u.clave,r.descripcion as rol from usuario u join rol r on u.rol_id = r.id_rol where u.estado= 'A'");
+	 $var_1 = $_GET["usuario"];
+
+	 $var_2 = $_GET["clave"];
+
+
+	$data = BDD::QUERY("SELECT u.id,u.usuario,u.clave,r.descripcion as rol from usuario u join rol r on u.rol_id = r.id_rol where u.estado= 'A'");
 	if ($data) {
-		echo json_encode($data, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_PRETTY_PRINT);
+
+		$array = array("usuario" => $data);
+		echo json_encode($array, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_PRETTY_PRINT);
 	}else{
 		echo json_encode("error", JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_PRETTY_PRINT);
 	}
